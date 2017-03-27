6d0a3058f520fb71ce17c73c665523d9b5943dbe
Atul Kumar Gupta <atul.svnit@yahoo.com>
2017-03-27 20:56:58 +0530

    Update settings.py

--- DocumentSearchEngine/settings.py
+++ DocumentSearchEngine/settings.py
@@ -125,18 +125,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-DATABASES['default'] = dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
-
-DEBUG = False
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
 
 
 # Static files (CSS, JavaScript, Images)
